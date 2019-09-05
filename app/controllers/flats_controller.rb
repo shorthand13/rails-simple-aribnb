20b0aae0fb83c6fbd9c5778f0b542b0152952174
Genki Kurihara <spiral809@gmail.com>
2019-09-06 01:04:54 +0900

    created controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,16 +3,38 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
   def new
     @flat = Flat.new
   end
 
   def create
     @flat = Flat.new(flat_params)
+    @flat.save
+
+    redirect_to flat_path(@flat)
   end
 
-  def show
+  def edit
     @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
